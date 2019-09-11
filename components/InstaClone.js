b20c407642385d16bc2d9508a4439ecb27fb4788
mark <markhinojosa1212@gmail.com>
2019-09-11 15:48:55 -0500

    worked on image styling

--- components/InstaClone.js
+++ components/InstaClone.js
@@ -4,13 +4,18 @@ import { Text, View, Image } from 'react-native'
 export default class InstaClone extends Component {
     render() {
         return (
-            <View style={{ flexDirection: 'column', borderColor: 'red', borderWidth: 3, justifyContent: 'center'}}>
-                <View>
+            <View style={{ flexDirection: 'column', borderColor: 'red', borderWidth: 3, height: '100%', width: '100%' }}>
+                <View style={{justifyContent:'center', margin:10}}>
+                    <Text style={{textAlign:'center'}}>Instagram</Text>
+                </View>
+                <View style={{ flexDirection: 'row', borderColor: 'yellow', borderWidth: 3 }}>
                     {/* <Image/> */}
                     <Text> MarkH </Text>
                     <Text>...</Text>
                 </View>
-                <Image style={{ resizeMode: 'contain', height: '100%', width: '100%', justifyContent: 'center', imageResizeMode: 'contain' }} source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/rn-instagramclone.appspot.com/o/puppy.png?alt=media&token=a7b798e1-fd04-41a2-aeb1-1b6646b9da61' }} />
+                <View style={{alignItems:'center'}}>
+                    <Image style={{ resizeMode: 'stretch', height: '50%', width: '100%', justifyContent: 'center' }} source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/rn-instagramclone.appspot.com/o/puppy.png?alt=media&token=a7b798e1-fd04-41a2-aeb1-1b6646b9da61' }} />
+                </View>
             </View>
         )
     }
