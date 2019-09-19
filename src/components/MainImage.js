a4c78bec80e85e8def6ffc82c78fe9a9768c6975
mark <markhinojosa1212@gmail.com>
2019-09-19 00:30:27 -0500

    heart color change on toggle on heart and image

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class MainImage extends Component {
     render() {
         return (
-            <View style={{
-                flexDirection: 'column',
-                borderWidth: 3,
-                borderColor: 'red',
-                height: '70%',
-                // width: '100%'
-            }}>
-                <Image style={{
-                    resizeMode: 'cover',
-                    height: '100%',
-                    width: '100%',
-                    justifyContent: 'center'
-                }}
-                    source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/rn-instagramclone.appspot.com/o/puppy.png?alt=media&token=a7b798e1-fd04-41a2-aeb1-1b6646b9da61' }} />
+            <View style={styles.mainImageContainer}>
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={() => this.props.imageLiked()}
+                >
+                    <Image style={styles.mainImage}
+                        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/rn-instagramclone.appspot.com/o/puppy.png?alt=media&token=a7b798e1-fd04-41a2-aeb1-1b6646b9da61' }} />
+                </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    mainImageContainer: {
+        flexDirection: 'column',
+        borderWidth: StyleSheet.hairlineWidth,
+        height: '65%',
+        width: '100%'
+    },
+    mainImage: {
+        resizeMode: 'cover',
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center'
+    }
+})
