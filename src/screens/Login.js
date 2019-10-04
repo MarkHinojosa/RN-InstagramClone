79d46a1d48332ac343fc56ab4349ac95f46037c3
mark <markhinojosa1212@gmail.com>
2019-10-03 22:18:29 -0500

    added login page styling

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-
-
+import { Text, View, TouchableOpacity, TextInput } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class Login extends Component {
 
@@ -20,20 +19,38 @@ export default class Login extends Component {
                 style={{
                     height: "100%",
                     width: "100%",
-                    alignItems: 'center',
-                    // justifyContent: 'center'
+                    flexDirection: 'column',
+                    flex: 1
                 }}>
-                <TouchableOpacity
-                    onPress={() => { this._login() }}
-                >
-                    <Text> Login  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => { this._newUser() }}
+                <LinearGradient style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }} colors={['#4c669f', '#3b5998', '#192f6a']}>
+                    <View
+                        style={{ width: '80%', height: '40%' }}
+                    >
+                        <TextInput
+                            placeholder={'username'}
+                            style={{ backgroundColor: 'white', borderRadius: 10, marginVertical: 10 }}
+                        />
+                        <TextInput
+                            placeholder={'password'}
+                            style={{ backgroundColor: 'white', borderRadius: 10, marginVertical: 10 }}
+                        />
+                        <View style={{ alignItems: 'flex-end' }}>
+                            <TouchableOpacity
+                                onPress={() => { this._login() }}
 
-                >
-                    <Text> New User? </Text>
-                </TouchableOpacity>
+                            >
+                                <Text style={{ color:'white', borderColor: 'gray', borderWidth: 1, borderRadius: 5, textAlign:'center'}}> Login  </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={() => { this._newUser() }}
+
+                    >
+                        <Text style={{color: 'white'}}> New User? </Text>
+                    </TouchableOpacity>
+                </LinearGradient>
             </View>
 
         )
