9416566032b3acceaa2e8e04fb75e5fa850a0772
mark <markhinojosa1212@gmail.com>
2019-10-17 23:54:50 -0500

    attempting to implement camera kit

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import {CameraKitCamera} from 'react-native-camera-kit';
 export default class Camera extends Component {
-    render() {
-        return (
-            <View style={{height: '100%'}}>
-                <Text> Camera Screen </Text>
-            </View>
-        )
+  //   componentDidMount = () => {
+  // const isUserAuthorizedCamera = await CameraKitCamera.requestDeviceCameraAuthorization();
+  //   };
+
+  renderCamera = () => {
+    const isFocused = this.props.navigation.isFocused();
+    if (!isFocused) {
+      return null;
+    } else if (isFocused) {
+      return (
+        <CameraKitCamera
+          ref={cam => (this.camera = cam)}
+          style={{
+            flex: 1,
+            backgroundColor: 'white',
+          }}
+          cameraOptions={{
+            flashMode: 'auto', // on/off/auto(default)
+            focusMode: 'on', // off/on(default)
+            zoomMode: 'on', // off/on(default)
+            ratioOverlay: '1:1', // optional, ratio overlay on the camera and crop the image seamlessly
+            ratioOverlayColor: '#00000077', // optional
+          }}
+          onReadQRCode={event =>
+            console.log(event.nativeEvent.qrcodeStringValue)
+          } // optional
+        />
+      );
     }
+  };
+
+  render() {
+    return <View>{this.renderCamera()}</View>;
+  }
 }
